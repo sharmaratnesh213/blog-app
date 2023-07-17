01c417853686675f3906fdde881f5d7e05db96fa
Ratnesh Kumar <sharmaratnesh213@gmail.com>
2023-07-17 10:56:03 +0530

    Home

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -5,7 +5,7 @@ import Post from '../post/Post'
 export default function Posts({ posts }) {
     return (
         <div className='posts'>
-            {posts.map(p => (
+            {posts.length == 0 ? <> </> : posts.map(p => (
                 <Post key={p._id} post={p} />
             ))}
         </div>
