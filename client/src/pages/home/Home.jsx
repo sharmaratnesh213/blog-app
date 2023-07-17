86254391cb1f8c3889ec46357a458d894a8afb3a
Ratnesh Kumar <sharmaratnesh213@gmail.com>
2023-07-17 14:58:06 +0530

    Update Home.jsx

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -14,7 +14,7 @@ export default function Home() {
 
     useEffect(() => {
         const fetchPosts = async () => {
-            const res = await axios.get(`${BASE_URL}/api/posts` + search);
+            const res = await axios.get(`${BASE_URL}/posts` + search);
             setPosts(res.data);
         };
         fetchPosts();
